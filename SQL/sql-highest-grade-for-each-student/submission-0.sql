98fae3d49a13d91af21f1dc96d1ec8b0776cc56d
Aman Negi <aman.work121@gmail.com>
2026-07-23 23:16:16 +0530

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,2 @@
+-- Write your query below
+select student_id, exam_id, score from (select *, row_number() over(partition by student_id order by score desc, exam_id asc) as rn from exam_results) t where rn=1 order by student_id;
