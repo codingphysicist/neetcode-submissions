dd1686d7d3df9b0c77857a376ee0cd96b0d2fa8c
Aman Negi <aman.work121@gmail.com>
2026-07-25 00:46:24 +0530

    Add: sql-evaluate-boolean-expression - submission-0

---
+++ SQL/sql-evaluate-boolean-expression/submission-0.sql
@@ -0,0 +1,2 @@
+-- Write your query below
+select left_operand, operator, right_operand, case when operator='>' and v1.value>v2.value then 'true' when operator='<' and v1.value<v2.value then 'true' when operator='=' and v1.value=v2.value then 'true' else 'false' end as value from expressions e join variables v1 on e.left_operand=v1.name join variables v2 on e.right_operand=v2.name;
